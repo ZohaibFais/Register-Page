ab5798cda2dd0d342a71de49a2a360b4eb3081ee
ZohaibFais <murtuza.tcc@gmail.com>
2024-03-07 16:59:15 +0500

    complete

--- script.js
+++ script.js
@@ -9,40 +9,38 @@ var updatesCheckbox = document.getElementById('updatesCheckbox');
 var submitButton = document.getElementById('submitButton');
 
 
-var usersData = [];
-
 function formSubmit() {
-
-    if (!emailInput.value || !fullNameInput.value || !countrySelect.value || !passwordInput.value || !confirmPasswordInput.value || !contactNumberInput.value || !termsCheckbox.checked) 
-    {
+    if (!emailInput.value || !fullNameInput.value || !countrySelect.value || !passwordInput.value || !confirmPasswordInput.value || !contactNumberInput.value || !termsCheckbox.checked) {
         alert('Fill in all Fields and accept the terms.');
+    } else if (passwordInput.value != confirmPasswordInput.value){
+        alert("Password does not match");
+    } else {
         
-    }
-    else if (passwordInput.value != confirmPasswordInput.value){
-        alert("Password does not match")
-    }
-    else{
+        var usersData = JSON.parse(localStorage.getItem('Data')) || []
+        
         var userData = {
-            email : emailInput.value,
-            fullname : fullNameInput.value,
-            country : countrySelect.value,
+            email: emailInput.value,
+            fullname: fullNameInput.value,
+            country: countrySelect.value,
             password: passwordInput.value,
             number: contactNumberInput.value,
-            update : updatesCheckbox.checked
-        }
-         var userData = JSON.stringify(userData)
-        usersData.push(userData)
-        localStorage.setItem("Data", usersData )
-        alert("Your account has been registered")
-    
-        emailInput.value = ""
-        fullNameInput.value = ""
-        countrySelect.value = ""
-        passwordInput.value = ""
-        confirmPasswordInput.value = ""
-        contactNumberInput.value = ""
+            update: updatesCheckbox.checked
+        };
 
+
+        usersData.push(userData);
+
+        localStorage.setItem('Data', JSON.stringify(usersData));
+        
+        emailInput.value = "";
+        fullNameInput.value = "";
+        countrySelect.value = "";
+        passwordInput.value = "";
+        confirmPasswordInput.value = "";
+        contactNumberInput.value = "";
+        termsCheckbox.checked = false;
+        updatesCheckbox.checked = false;
+
+        alert("Your account has been registered.");
     }
- 
 }
-
