fc4f772ea444da4877c534817d0863875e63588f
ZohaibFais <murtuza.tcc@gmail.com>
2024-03-09 16:06:21 +0500

    complete

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+// Login js
 
 var emailInput = document.getElementById('email');
 var passwordInput = document.getElementById('password');
+var appLogin = document.getElementById('appLogin');
+var appSignup = document.getElementById('appSignup');
+var islogged = false;
 
 function login() {
     var email = emailInput.value;
@@ -8,38 +12,48 @@ function login() {
 
     var usersData = JSON.parse(localStorage.getItem('Data')) || [];
 
-    var loginuser = usersData.find(function(activeuser){
-       return  activeuser.email == email & activeuser.password == password
-    })
     var loggedInUser = usersData.find(function(user) {
-        return user.email === email && user.password === password;
+        return user.email == email && user.password == password;
     });
 
     if (loggedInUser) {
         alert("Login successful. Welcome, " + loggedInUser.fullname + "!");
         emailInput.value = "";
         passwordInput.value = "";
-        window.location.href = "http://127.0.0.1:5500/app.html"
+        islogged = true;
+        window.location.href = "http://127.0.0.1:5500/app.html";
+    
+        appLogin.style.visibility = 'hidden';
+        appSignup.innerHTML = "Sign out"
     } else {
         alert("Invalid email or password. Please try again.");
-
-
+        islogged = false;
     }
-    
 }
+appLogin.style.visibility = 'hidden';
+appSignup.innerHTML = "Sign out"
 
 
-var emailInput = document.getElementById('email');
-var fullNameInput = document.getElementById('fullName');
-var countrySelect = document.getElementById('countryName');
-var passwordInput = document.getElementById('password');
-var confirmPasswordInput = document.getElementById('confirmPassword');
-var contactNumberInput = document.getElementById('contactNumber');
-var termsCheckbox = document.getElementById('termsCheckbox');
-var updatesCheckbox = document.getElementById('updatesCheckbox');
-var submitButton = document.getElementById('submitButton');
+// App signout 
+
+    function signout(){
+        window.location.href = "http://127.0.0.1:5500/login.html"
+    }
+
+
+
+// Register page 
 
 function formSubmit()  {
+    var emailInput = document.getElementById('email');
+    var fullNameInput = document.getElementById('fullName');
+    var countrySelect = document.getElementById('countryName');
+    var passwordInput = document.getElementById('password');
+    var confirmPasswordInput = document.getElementById('confirmPassword');
+    var contactNumberInput = document.getElementById('contactNumber');
+    var termsCheckbox = document.getElementById('termsCheckbox');
+    var updatesCheckbox = document.getElementById('updatesCheckbox');
+    var submitButton = document.getElementById('submitButton');
     if (!emailInput.value || !fullNameInput.value || !countrySelect.value || !passwordInput.value || !confirmPasswordInput.value || !contactNumberInput.value || !termsCheckbox.checked) {
         alert('Fill in all Fields and accept the terms.');
     } else if (passwordInput.value != confirmPasswordInput.value){
@@ -73,4 +87,9 @@ function formSubmit()  {
 
         alert("Your account has been registered.");
     }
+}
+
+
+function alreadyuser(){
+    window.location.href = "http://127.0.0.1:5500/login.html"
 }
